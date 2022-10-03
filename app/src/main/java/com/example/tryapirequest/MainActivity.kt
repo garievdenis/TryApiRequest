0ae6ddbdb821e2490529e9b45efa3681cb9365b2
garievdenis <gariev@webmx.ru>
2022-10-03 23:19:05 +0500

    добÑавadd comments for description code(

--- app/src/main/java/com/example/tryapirequest/MainActivity.kt
+++ app/src/main/java/com/example/tryapirequest/MainActivity.kt
@@ -22,22 +22,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         tvMain = findViewById(R.id.tvMain)
         button = findViewById(R.id.btn)
-
+        /*Инициальизация исполнителя для потока*/
         val executorService: ExecutorService = Executors.newSingleThreadExecutor()
 
         button.text = "Get fact about random year"
         button.setOnClickListener {
+            /*получение данных из потока для отображения в textView*/
             tvMain.text = executorService.submit(Callable {
                 httpRequest("http://numbersapi.com/random/year")
             }).get()
         }
     }
-
+    /*Функция для работы в отдельном потоке*/
     @Throws(IOException::class)
     fun httpRequest(urlString: String):String {
+        /*Созддание экземпляра URL*/
         val url = URL(urlString)
+        /*Создание экзепляра класса для соедниения по протоколу HTTP*/
         val connection = url.openConnection() as HttpURLConnection
+        /*Установка метода запроса*/
         connection.requestMethod = "GET"
+        /*Отправка запроса и чтение полученых данных*/
         var data: Int = connection.inputStream.read()
         var str = ""
         while (data != -1){
